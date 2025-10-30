a3a0e6ff49e4ba6e44f64f242c13944d9f2f8722
melan-Akash <user.melan.akash@icloud.com>
2025-10-30 20:22:51 +0530

    4th commite

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Markdown from 'react-markdown'
 
 const CreationItem = ({ item }) => {
+
+  const [expanded, setExpanded] = useState(false)
+
   return (
-    <div className='p-4 max-w-5xl w-full bg-white border border-gray-200 rounded-lg cursor-pointer hover:-translate-y-1 transition-all duration-200'>
+    <div onClick={()=>setExpanded(!expanded)} className='p-4 max-w-5xl w-full bg-white border border-gray-200 rounded-lg cursor-pointer hover:-translate-y-1 transition-all duration-200'>
       <div className='flex justify-between items-center gap-4'>
         <div>
           <h2 className='font-medium text-gray-700'>{item.prompt}</h2>
           <p className='text-gray-500 text-sm'>
-            {item.type} • {new Date(item.createdAt).toLocaleDateString()}
+            {item.type} • {new Date(item.created_at).toLocaleDateString()}
           </p>
         </div>
         <button className='bg-[#EFF6FF] border border-[#BFDBFE] text-[#1E40AF] px-4 py-1 rounded-full text-xs font-medium'>
           {item.type}
         </button>
       </div>
+      {
+        expanded && (
+          <div>
+            {item.type === 'image' ? (
+              <div>
+                <img src={item.content} alt="" className='mt-5 w-full max-w-md'/>
+              </div>
+            ) : 
+            <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-700'>
+              <div className='reset-tw'>
+                <Markdown> 
+                  {item.content}
+                </Markdown> 
+
+              </div>
+              
+
+            </div>
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
